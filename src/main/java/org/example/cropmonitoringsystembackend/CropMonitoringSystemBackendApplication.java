b414d71ca4fbbc66dacdd8d85134c74e3a84e128
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-10-31 17:45:10 +0530

    initial commit

---
+++ src/main/java/org/example/cropmonitoringsystembackend/CropMonitoringSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.cropmonitoringsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropMonitoringSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CropMonitoringSystemBackendApplication.class, args);
+    }
+
+}

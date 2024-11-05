1bea5dd10d2726edb339fb99eda23f4ee470d876
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-05 18:55:26 +0530

    success health check

--- src/main/java/org/example/cropmonitoringsystembackend/controller/HealthCheckController.java
+++ src/main/java/org/example/cropmonitoringsystembackend/controller/HealthCheckController.java
@@ -1,9 +1,15 @@
 package org.example.cropmonitoringsystembackend.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/health")
 public class HealthCheckController {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public String healthTest() {
+        return "Crop Monitoring System is working..";
+    }
 }

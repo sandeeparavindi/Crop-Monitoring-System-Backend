d365ab02477027910e785cdecb668665f13275f5
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-05 18:53:17 +0530

    add health check

---
+++ src/main/java/org/example/cropmonitoringsystembackend/controller/FieldController.java
@@ -0,0 +1,9 @@
+package org.example.cropmonitoringsystembackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/v1/fields")
+public class FieldController {
+}

ddb524fc7f0081b6ae15f5e81ebf213c6ef9dacb
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-08 20:55:11 +0530

    implement save crop in serviceImpl

--- src/main/java/org/example/cropmonitoringsystembackend/service/impl/CropServiceIMPL.java
+++ src/main/java/org/example/cropmonitoringsystembackend/service/impl/CropServiceIMPL.java
@@ -2,8 +2,12 @@ package org.example.cropmonitoringsystembackend.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.example.cropmonitoringsystembackend.customObj.CropResponse;
+import org.example.cropmonitoringsystembackend.dao.CropDAO;
 import org.example.cropmonitoringsystembackend.dto.impl.CropDTO;
+import org.example.cropmonitoringsystembackend.entity.impl.Crop;
+import org.example.cropmonitoringsystembackend.exception.DataPersistException;
 import org.example.cropmonitoringsystembackend.service.CropService;
+import org.example.cropmonitoringsystembackend.util.Mapping;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +17,18 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class CropServiceIMPL implements CropService {
+    private final CropDAO cropDAO;
+    private final Mapping mapping;
     @Override
     public void saveCrop(CropDTO cropDTO) {
-
+        Crop saveCrop = cropDAO.save(mapping.convertToCrop(cropDTO));
+        try{
+            if(saveCrop == null){
+                throw new DataPersistException("Can't save Crop");
+            }
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override

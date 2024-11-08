a3fa97e47565e522e86809e75d47886ac9145b81
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-08 18:23:24 +0530

    exception for crop not found

---
+++ src/main/java/org/example/cropmonitoringsystembackend/exception/CropNotFoundException.java
@@ -0,0 +1,6 @@
+package org.example.cropmonitoringsystembackend.exception;
+
+public class CropNotFoundException extends RuntimeException {
+    public CropNotFoundException(String message) {
+    }
+}

9556fe70dfeec3a6b32800cb2cf2ac9882ff6288
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-10 16:04:16 +0530

    success vehicle controller

--- src/main/java/org/example/cropmonitoringsystembackend/controller/VehicleController.java
+++ src/main/java/org/example/cropmonitoringsystembackend/controller/VehicleController.java
@@ -3,12 +3,15 @@ package org.example.cropmonitoringsystembackend.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.cropmonitoringsystembackend.dto.impl.VehicleDTO;
 import org.example.cropmonitoringsystembackend.exception.DataPersistException;
+import org.example.cropmonitoringsystembackend.exception.VehicleNotFoundException;
 import org.example.cropmonitoringsystembackend.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1/vehicles")
@@ -28,6 +31,48 @@ public class VehicleController {
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping(value = "allVehicles", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<VehicleDTO> getAllVehicles() {
+        return vehicleService.getAllVehicles();
+    }
+
+    @DeleteMapping(value = "/{code}")
+    public ResponseEntity<Void> deleteSelectedVehicle(@PathVariable("code") String code) {
+        try {
+            vehicleService.deleteVehicle(code);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (VehicleNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<VehicleDTO>> searchVehicles(
+            @RequestParam(value = "vehicleCode", required = false) String vehicleCode,
+            @RequestParam(value = "vehicleCategory", required = false) String vehicleCategory) {
+        List<VehicleDTO> crops = vehicleService.searchVehicles(vehicleCode, vehicleCategory);
+        return new ResponseEntity<>(crops, HttpStatus.OK);
+    }
+
+
+    @PatchMapping(value = "/{vehicleCode}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateSelectedVehicle(
+            @PathVariable("vehicleCode") String vehicleCode,
+            @RequestBody VehicleDTO vehicleDTO
+    ) {
+        try {
+            vehicleService.updateVehicle(vehicleCode, vehicleDTO);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (VehicleNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }

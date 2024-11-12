3cd6a9f51d95963745fbb1c6b2bb4df038331e71
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-12 11:36:24 +0530

    staff services

---
+++ src/main/java/org/example/cropmonitoringsystembackend/service/StaffService.java
@@ -0,0 +1,13 @@
+package org.example.cropmonitoringsystembackend.service;
+
+import org.example.cropmonitoringsystembackend.dto.impl.StaffDTO;
+
+import java.util.List;
+
+public interface StaffService {
+    void saveStaff(StaffDTO staffDTO);
+    List<StaffDTO> getAllStaffs();
+    void deleteStaff(String id);
+    void updateStaff(String id, StaffDTO staffDTO);
+    List<StaffDTO> searchStaff  (String searchTerm);
+}

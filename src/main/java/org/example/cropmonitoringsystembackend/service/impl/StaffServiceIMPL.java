dc76ddaa3aabed2c3973e0a60b79efae1ec04733
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-12 11:52:47 +0530

    staff save impl

--- src/main/java/org/example/cropmonitoringsystembackend/service/impl/StaffServiceIMPL.java
+++ src/main/java/org/example/cropmonitoringsystembackend/service/impl/StaffServiceIMPL.java
@@ -1,8 +1,15 @@
 package org.example.cropmonitoringsystembackend.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.example.cropmonitoringsystembackend.dao.StaffDAO;
+import org.example.cropmonitoringsystembackend.dao.VehicleDAO;
 import org.example.cropmonitoringsystembackend.dto.impl.StaffDTO;
+import org.example.cropmonitoringsystembackend.entity.impl.Crop;
+import org.example.cropmonitoringsystembackend.entity.impl.Staff;
+import org.example.cropmonitoringsystembackend.entity.impl.Vehicle;
+import org.example.cropmonitoringsystembackend.exception.DataPersistException;
 import org.example.cropmonitoringsystembackend.service.StaffService;
+import org.example.cropmonitoringsystembackend.util.Mapping;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +19,23 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class StaffServiceIMPL implements StaffService {
+    private final StaffDAO staffDAO;
+    private final VehicleDAO vehicleDAO;
+    private final Mapping mapping;
     @Override
     public void saveStaff(StaffDTO staffDTO) {
-
+        Vehicle vehicle = vehicleDAO.findById(staffDTO.getVehicleCode())
+                .orElseThrow(() -> new DataPersistException("Invalid Vehicle code"));
+        Staff staff = mapping.convertToStaff(staffDTO);
+        staff.setVehicle(vehicle);
+        Staff savedStaff = staffDAO.save(staff);
+        try {
+            if (savedStaff == null) {
+                throw new DataPersistException("Can't save Staff");
+            }
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override

3416fa5e6e513ce0e06dab9d248665ba030dcb3e
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-12 11:05:19 +0530

    add staff not found exception

---
+++ src/main/java/org/example/cropmonitoringsystembackend/exception/StaffMemberNotFoundException.java
@@ -0,0 +1,6 @@
+package org.example.cropmonitoringsystembackend.exception;
+
+public class StaffMemberNotFoundException extends RuntimeException {
+    public StaffMemberNotFoundException(String message) {
+    }
+}

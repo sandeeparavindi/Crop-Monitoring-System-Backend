2a2c27423d192c635683fea902337623f020d68d
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-12 20:01:36 +0530

    implement search query

--- src/main/java/org/example/cropmonitoringsystembackend/dao/StaffDAO.java
+++ src/main/java/org/example/cropmonitoringsystembackend/dao/StaffDAO.java
@@ -2,8 +2,14 @@ package org.example.cropmonitoringsystembackend.dao;
 
 import org.example.cropmonitoringsystembackend.entity.impl.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StaffDAO extends JpaRepository<Staff, String> {
+    @Query("SELECT s FROM Staff s WHERE s.id = :searchTerm OR s.firstName = :searchTerm")
+    List<Staff> findByIdOrFirstName(@Param("searchTerm") String searchTerm);
 }

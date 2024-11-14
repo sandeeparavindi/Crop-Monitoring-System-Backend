371aa5e90c0cb0acac390d5e0aea64b972f8f2b8
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-14 11:26:20 +0530

    equipment get, search, update, delete success

--- src/main/java/org/example/cropmonitoringsystembackend/controller/EquipmentController.java
+++ src/main/java/org/example/cropmonitoringsystembackend/controller/EquipmentController.java
@@ -2,12 +2,16 @@ package org.example.cropmonitoringsystembackend.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.cropmonitoringsystembackend.dto.impl.EquipmentDTO;
+import org.example.cropmonitoringsystembackend.exception.CropNotFoundException;
 import org.example.cropmonitoringsystembackend.exception.DataPersistException;
+import org.example.cropmonitoringsystembackend.exception.EquipmentNotFoundException;
 import org.example.cropmonitoringsystembackend.service.EquipmentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1/equipment")
@@ -29,6 +33,43 @@ public class EquipmentController {
             } catch (Exception e) {
                 return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
+        }
+    }
+
+    @GetMapping(value = "allEquipment", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<EquipmentDTO> getAllEquipment() {
+        return equipmentService.getAllEquipment();
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<EquipmentDTO>> searchEquipments(@RequestParam(value = "searchTerm", required = false) String searchTerm) {
+        List<EquipmentDTO> equipments = equipmentService.searchEquipment(searchTerm);
+        return new ResponseEntity<>(equipments, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteSelectedCrop(@PathVariable("id") String id) {
+        try {
+            equipmentService.deleteEquipment(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (CropNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PatchMapping(value = "/{equipmentId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateSelectedEquipment(@PathVariable("equipmentId") String equipmentId,
+                                                        @RequestBody EquipmentDTO equipmentDTO) {
+        try {
+            equipmentService.updateEquipment(equipmentId, equipmentDTO);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (EquipmentNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }

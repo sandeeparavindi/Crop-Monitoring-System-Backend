06d0b2f7c2d794d11dd0b22c7fa0e4d9497742d9
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-17 09:53:52 +0530

    handle sign in sign up request

---
+++ src/main/java/org/example/cropmonitoringsystembackend/auth/request/SignUpRequest.java
@@ -0,0 +1,16 @@
+package org.example.cropmonitoringsystembackend.auth.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.cropmonitoringsystembackend.util.Role;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SignUpRequest {
+    private String email;
+    private String password;
+    private Role role;
+}

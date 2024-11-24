1ff558f2c4e2468df4d1a01ca44b248858547b32
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-24 16:37:37 +0530

    service exception

---
+++ src/main/java/org/example/cropmonitoringsystembackend/exception/ServiceException.java
@@ -0,0 +1,7 @@
+package org.example.cropmonitoringsystembackend.exception;
+
+public class ServiceException extends RuntimeException {
+    public ServiceException(String message) {
+        super(message);
+    }
+}

84714d78ac82e03d084b5fb22fab402a7e906ecc
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-24 16:53:48 +0530

    authentication service

---
+++ src/main/java/org/example/cropmonitoringsystembackend/service/AuthenticationService.java
@@ -0,0 +1,11 @@
+package org.example.cropmonitoringsystembackend.service;
+
+import org.example.cropmonitoringsystembackend.auth.request.SignInRequest;
+import org.example.cropmonitoringsystembackend.auth.request.SignUpRequest;
+import org.example.cropmonitoringsystembackend.auth.response.JWTAuthResponse;
+
+public interface AuthenticationService {
+    JWTAuthResponse signIn(SignInRequest signInRequest);
+    JWTAuthResponse signUp(SignUpRequest signUpRequest);
+    JWTAuthResponse refreshToken(String accessToken);
+}

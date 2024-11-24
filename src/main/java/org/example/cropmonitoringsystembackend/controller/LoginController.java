5067486138ccce255ee895207ece210a132d48a4
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-24 17:08:04 +0530

    controllers for login

---
+++ src/main/java/org/example/cropmonitoringsystembackend/controller/LoginController.java
@@ -0,0 +1,38 @@
+package org.example.cropmonitoringsystembackend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.cropmonitoringsystembackend.auth.request.SignInRequest;
+import org.example.cropmonitoringsystembackend.auth.request.SignUpRequest;
+import org.example.cropmonitoringsystembackend.auth.response.JWTAuthResponse;
+import org.example.cropmonitoringsystembackend.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v0/auth")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://127.0.0.1:5500")
+public class LoginController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<JWTAuthResponse> signIn(
+            @RequestBody SignInRequest signInRequest){
+        System.out.println("Signing in");
+        return ResponseEntity.ok(
+                authenticationService.signIn(signInRequest));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<JWTAuthResponse> signUp(
+            @RequestBody SignUpRequest signUpRequest){
+        return ResponseEntity.ok(
+                authenticationService.signUp(signUpRequest));
+    }
+
+
+    @PostMapping("refresh")
+    public ResponseEntity<JWTAuthResponse> refreshToken (@RequestParam ("refreshToken") String refreshToken) {
+        return ResponseEntity.ok(authenticationService.refreshToken(refreshToken));
+    }
+}

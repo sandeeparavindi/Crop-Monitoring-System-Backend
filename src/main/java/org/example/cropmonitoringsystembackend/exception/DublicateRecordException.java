e8f266e562cd8e66ab6bbcf646b7a4171db77d5e
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-24 16:35:58 +0530

    duplicate record exception

---
+++ src/main/java/org/example/cropmonitoringsystembackend/exception/DublicateRecordException.java
@@ -0,0 +1,7 @@
+package org.example.cropmonitoringsystembackend.exception;
+
+public class DublicateRecordException extends ServiceException {
+    public DublicateRecordException(String message) {
+        super(message);
+    }
+}

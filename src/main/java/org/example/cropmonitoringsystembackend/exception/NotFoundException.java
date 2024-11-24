f998f609af5260add3d1530204b2d9629cd033e6
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-24 16:36:51 +0530

    not found exception

---
+++ src/main/java/org/example/cropmonitoringsystembackend/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.cropmonitoringsystembackend.exception;
+
+public class NotFoundException extends ServiceException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}

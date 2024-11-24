35fdea18e23a74d9d36f88e2820507cf2f484270
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-24 16:39:54 +0530

    dto for user

--- src/main/java/org/example/cropmonitoringsystembackend/dto/impl/UserDTO.java
+++ src/main/java/org/example/cropmonitoringsystembackend/dto/impl/UserDTO.java
@@ -6,11 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.cropmonitoringsystembackend.util.Role;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class UserDTO {
+public class UserDTO implements Serializable {
     private String email;
     private String password;
     private Role role;

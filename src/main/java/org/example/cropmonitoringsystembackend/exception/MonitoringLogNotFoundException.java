7996584c340a8b29eb34043bb92a40fcf9c357a6
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-11-25 12:17:52 +0530

    monitoring log not found exception

---
+++ src/main/java/org/example/cropmonitoringsystembackend/exception/MonitoringLogNotFoundException.java
@@ -0,0 +1,6 @@
+package org.example.cropmonitoringsystembackend.exception;
+
+public class MonitoringLogNotFoundException extends RuntimeException {
+    public MonitoringLogNotFoundException(String message) {
+    }
+}

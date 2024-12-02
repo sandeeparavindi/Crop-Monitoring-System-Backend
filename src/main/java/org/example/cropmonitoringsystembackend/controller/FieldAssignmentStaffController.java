ecf515e02e6029625c756e24b0564d4c466f0a74
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-12-03 00:09:48 +0530

    assignment get all end point

--- src/main/java/org/example/cropmonitoringsystembackend/controller/FieldAssignmentStaffController.java
+++ src/main/java/org/example/cropmonitoringsystembackend/controller/FieldAssignmentStaffController.java
@@ -3,9 +3,12 @@ package org.example.cropmonitoringsystembackend.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.cropmonitoringsystembackend.dto.impl.FieldStaffAssignmentDTO;
 import org.example.cropmonitoringsystembackend.service.FieldAssignmentStaffService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1/assignment")
@@ -20,5 +23,10 @@ public class FieldAssignmentStaffController {
     public ResponseEntity<String> saveAssignment(@RequestBody FieldStaffAssignmentDTO assignmentDTO) {
         fieldAssignmentStaffService.saveAssignment(assignmentDTO);
         return ResponseEntity.ok("Assignment saved successfully");
+    }
+
+    @GetMapping(value = "allassignments", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<FieldStaffAssignmentDTO> getAllAssignments() {
+        return fieldAssignmentStaffService.getAllFieldStaffAssignments();
     }
 }

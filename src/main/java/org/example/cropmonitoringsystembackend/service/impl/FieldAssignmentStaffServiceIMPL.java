4fdfe580e2cb13db67c59ca6e6fe0c998b5ecd4e
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-12-03 01:05:42 +0530

    assignment field to staff member all success

--- src/main/java/org/example/cropmonitoringsystembackend/service/impl/FieldAssignmentStaffServiceIMPL.java
+++ src/main/java/org/example/cropmonitoringsystembackend/service/impl/FieldAssignmentStaffServiceIMPL.java
@@ -7,7 +7,6 @@ import org.example.cropmonitoringsystembackend.dao.StaffDAO;
 import org.example.cropmonitoringsystembackend.dto.impl.FieldStaffAssignmentDTO;
 import org.example.cropmonitoringsystembackend.entity.impl.Field;
 import org.example.cropmonitoringsystembackend.entity.impl.FieldStaffAssignment;
-import org.example.cropmonitoringsystembackend.entity.impl.MonitoringLog;
 import org.example.cropmonitoringsystembackend.entity.impl.Staff;
 import org.example.cropmonitoringsystembackend.service.FieldAssignmentStaffService;
 import org.example.cropmonitoringsystembackend.util.Mapping;

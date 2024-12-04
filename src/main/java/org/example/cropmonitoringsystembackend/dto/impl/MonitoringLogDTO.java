be21eabc776a857c99c58b86ad4e7f307034107b
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-12-04 14:18:25 +0530

    monitoring log validate

--- src/main/java/org/example/cropmonitoringsystembackend/dto/impl/MonitoringLogDTO.java
+++ src/main/java/org/example/cropmonitoringsystembackend/dto/impl/MonitoringLogDTO.java
@@ -1,5 +1,6 @@
 package org.example.cropmonitoringsystembackend.dto.impl;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,11 @@ import org.example.cropmonitoringsystembackend.dto.SuperDTO;
 @NoArgsConstructor
 @Data
 public class MonitoringLogDTO implements SuperDTO, MonitoringLogResponse {
+    @NotBlank(message = "Monitoring log code Cannot Be Null")
     private String log_code;
+    @NotBlank(message = "Monitoring log date Cannot Be Null")
     private String log_date;
+    @NotBlank(message = "Monitoring log observation Cannot Be Null")
     private String Observation;
     private String log_image;
     private String fieldCode;

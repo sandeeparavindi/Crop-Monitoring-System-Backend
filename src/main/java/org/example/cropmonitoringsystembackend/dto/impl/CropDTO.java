20b14f03028a9d865be0fafbfcaa20aa91b7372e
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-12-04 14:11:28 +0530

    crop validate

--- src/main/java/org/example/cropmonitoringsystembackend/dto/impl/CropDTO.java
+++ src/main/java/org/example/cropmonitoringsystembackend/dto/impl/CropDTO.java
@@ -1,5 +1,7 @@
 package org.example.cropmonitoringsystembackend.dto.impl;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +15,13 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 public class CropDTO implements CropStatus, CropResponse {
+    @NotBlank(message = "Crop crop Cannot Be Null")
     private String cropCode;
+    @NotBlank(message = "Crop Name Cannot Be Null")
+    @Pattern(regexp = "^[A-Za-z0-9]", message = "Common Name not valid")
     private String cropCommonName;
+    @NotBlank(message = "Crop Scientific Name Cannot Be Null")
+    @Pattern(regexp = "^[A-Za-z0-9]", message = "Scientific Name not valid")
     private String cropScientificName;
     private String category;
     private String cropSeason;
